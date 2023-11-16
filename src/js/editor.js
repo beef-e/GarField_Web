7c0c3b1a66cd0c1af1ad37c1fe4ac151be2797f0
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2023-11-16 18:30:02 +0100

    Added Shortcuts array

--- src/js/editor.js
+++ src/js/editor.js
@@ -4,6 +4,8 @@ import Paragraph from '@editorjs/paragraph';
 import Delimiter from '@editorjs/delimiter';
 import NestedList from '@editorjs/nested-list';*/
 
+const shortcuts = ['ALT+SHIFT+H', 'ALT+SHIFT+P', 'ALT+SHIFT+D', 'ALT+SHIFT+L'];
+
 const editor = new EditorJS({
 	tools: {
 		header: {
@@ -11,17 +13,21 @@ const editor = new EditorJS({
 			levels: [2, 3, 4],
 			defaultLevel: 2,
 			inlineToolbar: false,
+			shortcut: shortcuts[0],
 		},
 		paragraph: {
 			class: Paragraph,
 			inlineToolbar: false,
+			shortcut: shortcuts[1],
 		},
 		delimiter: {
 			class: Delimiter,
+			shortcut: shortcuts[2],
 		},
 		list: {
 			class: NestedList,
 			inlineToolbar: false,
+			shortcut: shortcuts[3],
 		},
 	},
 });
