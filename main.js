9d903526e3207a78c491a6c472449dfe3e30fc8d
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2023-11-27 17:22:47 +0100

    Added Switch mode menu button

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 const { app, BrowserWindow, Menu } = require('electron');
+let modeVar = true;
+
+const switchModeTrigger = () => {
+	console.log('switch mode');
+	console.log(modeVar);
+	modeVar = !modeVar;
+	/*BrowserWindow.getAllWindows().forEach((win) => {
+		if (mode) win.loadFile('index.html');
+		else win.loadFile('cornell.html');
+	});*/
+	createWindow(modeVar);
+	chiudiUltimaFinestra();
+	//app.relaunch();
+	//app.quit();
+};
 
 try {
 	require('electron-reloader')(module);
 } catch {}
 
-const createWindow = () => {
+const createWindow = (modeVar) => {
 	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
 	});
 
-	win.loadFile('index.html');
+	if (modeVar == true || modeVar == undefined) {
+		win.loadFile('index.html');
+	} else {
+		win.loadFile('cornell.html');
+	}
+
+	//win.loadFile('index.html');
 };
 
 app.whenReady().then(() => {
-	createWindow();
+	createWindow(modeVar);
 
 	app.on('window-all-closed', () => {
 		if (process.platform !== 'darwin') app.quit();
@@ -28,7 +49,16 @@ app.whenReady().then(() => {
 const menuTemplate = [
 	{
 		label: 'File',
-		submenu: [{ role: 'quit', accelerator: 'CmdOrCtrl+w' }],
+		submenu: [
+			{ role: 'quit', accelerator: 'CmdOrCtrl+w' },
+			{
+				label: 'Switch mode',
+				click: () => {
+					switchModeTrigger();
+				},
+				accelerator: 'CmdOrCtrl+Shift+m',
+			},
+		],
 	},
 
 	{
@@ -66,3 +96,11 @@ const menuTemplate = [
 
 const menu = Menu.buildFromTemplate(menuTemplate);
 Menu.setApplicationMenu(menu);
+
+const chiudiUltimaFinestra = () => {
+	for (let i = 0; i < BrowserWindow.getAllWindows().length; i++) {
+		if (i == BrowserWindow.getAllWindows().length - 1) {
+			BrowserWindow.getAllWindows()[i].close();
+		}
+	}
+};
